6196a704b7e227eb0ba21db71ab79876fceb9ef7
bayaprakash <cbaya.reddy8@gmail.com>
2021-08-30 17:56:51 +0530

    firstcommit

---
+++ src/main/java/org/example/quickstart/App.java
@@ -0,0 +1,13 @@
+package org.example.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
